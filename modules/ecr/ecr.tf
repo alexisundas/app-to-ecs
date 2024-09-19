068f71a97048fb399dc5d03ace97dfc9b5f157e9
Alexis <55557683+alexisundas@users.noreply.github.com>
2024-09-19 23:36:14 +0300

    Update ecr.tf

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -1,11 +1,3 @@
-# Check if the ECR repository already exists
-data "aws_ecr_repository" "existing_repo" {
-  name = "counting-app"
-}
-
-# Conditional resource creation using the count feature
 resource "aws_ecr_repository" "counting_app" {
-  count = length(data.aws_ecr_repository.existing_repo.repositories) == 0 ? 1 : 0
-
-  name = "counting-app"
+  name = var.ecr_repo_name
 }
